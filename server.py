e12e32e8c3c28b1354f5a0a96b120b216133a144
nk521 <nk_mason@protonmail.com>
2022-04-15 16:02:53 +0530

    server.py is a CLI app now * `create-superuser` - makes a new superuser with command line arguments * `run` - runs the server

--- server.py
+++ server.py
@@ -3,19 +3,24 @@ from pathlib import Path
 from typing import Any, MutableMapping
 
 import toml
+import typer
 import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from tortoise.contrib.starlette import register_tortoise
+from tortoise import Tortoise, run_async
 
 from aerich_config import DATABASE_URL
 from api.apiv1.all_routers import api_router
+from models.user import User
+from models.rti import Rti
+from utils import utils
 
 path_root = Path(__file__).parents[0]
 sys.path.append(str(path_root))
 
 app: FastAPI = FastAPI()
-
+typer_app: typer.Typer = typer.Typer()
 
 config: MutableMapping[str, Any] = toml.load("config.toml")
 
@@ -40,7 +45,7 @@ app.add_middleware(
 
 
 @app.get("/RTIApiv1/hello", summary="Test Api Call")
-async def TestApi():
+async def test_api():
     """
     Test API
 
@@ -51,12 +56,42 @@ async def TestApi():
 
 app.include_router(api_router, prefix="/apiv1")
 
-if config["debug"]["set"]:
-    if __name__ == "__main__":
-        uvicorn.Config(
-            reload=True,
-            app=app,
-            host=config["debug"]["host"],
-            port=config["debug"]["port"],
+
+@typer_app.command()
+def run() -> None:
+    if config["debug"]["set"]:
+        if __name__ == "__main__":
+            uvicorn.Config(
+                reload=True,
+                app=app,
+                host=config["debug"]["host"],
+                port=config["debug"]["port"],
+            )
+            uvicorn.run(app)
+
+
+@typer_app.command()
+def create_superuser(
+    username: str = typer.Option("", help="Username of the new superuser"),
+    email: str = typer.Option("", help="Email of the new superuser"),
+    password: str = typer.Option("", help="Password of the new superuser"),
+) -> None:
+    async def _run():
+        await Tortoise.init(
+            db_url=DATABASE_URL,
+            modules={"models": ["models"]},
         )
-        uvicorn.run(app, host="0.0.0.0", port=8080)
+        userObj = await User.create(
+            username=username,
+            email=email,
+            hashed_password=utils.GetPasswordHash(password),
+            is_active=True,
+            is_superuser=True,
+        )
+        await userObj.save()
+
+    run_async(_run())
+
+
+if __name__ == "__main__":
+    typer_app()
