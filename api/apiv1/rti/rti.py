945d7c222fdbc86e1f23b2972e6048b70499fcaf
nk521 <nk_mason@protonmail.com>
2022-04-15 16:03:51 +0530

    rti related apis

--- api/apiv1/rti/rti.py
+++ api/apiv1/rti/rti.py
@@ -1,19 +1,28 @@
 from datetime import datetime
 from typing import Any, List, MutableMapping
 
-from pydantic import EmailStr
-
 import api.utils
+import toml
 from api import deps
-from fastapi import APIRouter, Depends, HTTPException, UploadFile, Form, File
+from fastapi import APIRouter, Depends, File, Form, HTTPException, UploadFile
 from models import Rti
 from models.file_upload import FileUpload
+from pydantic import EmailStr
 from schemas import rti
-import toml
 
 router = APIRouter()
 
 config: MutableMapping[str, Any] = toml.load("config.toml")
+
+
+@router.get("/get/{id}")
+async def get_rti(id: str) -> Any:
+    if not await Rti.exists(id=id):
+        raise HTTPException(status_code=404, detail="Rti not found")
+
+    RtiObj: Rti = await Rti.get(id=id)
+    return api.utils.remove_meta_rti(dict(RtiObj))
+
 
 @router.post("/add")
 async def create_rti(
@@ -39,26 +48,59 @@ async def create_rti(
         FileUploadObj = await FileUpload.get(id=content.file)
         _kwargs["file"] = FileUploadObj
 
-    RtiObj: Rti = await Rti.create(
-        **_kwargs
-    )
+    RtiObj: Rti = await Rti.create(**_kwargs)
 
     await RtiObj.save()
 
     return api.utils.remove_meta_rti(dict(RtiObj))
 
 
-@router.post("/delete")
+@router.put("/update")
+async def update_rti(
+    content: rti.RtiUpdateIn, current_user=Depends(deps.get_current_active_user)
+) -> Any:
+
+    if not await Rti.exists(id=content.id):
+        raise HTTPException(status_code=404, detail="Rti not found")
+
+    RtiObj: Rti = await Rti.get(id=content.id)
+
+    _rti_send_date = datetime.fromtimestamp(int(content.rti_send_date))
+
+    _kwargs = {
+        "rti_send_date": _rti_send_date,
+        "registration_number": content.registration_number,
+        "name_of_sender": content.name_of_sender,
+        "email_of_sender": content.email_of_sender,
+        "ministry": content.ministry,
+        "public_authority": content.public_authority,
+        "query": content.query,
+        "created_by": current_user,
+        # topic=content.topic
+    }
+
+    FileUploadObjExists = await FileUpload.exists(id=content.file)
+    if FileUploadObjExists:
+        FileUploadObj = await FileUpload.get(id=content.file)
+        _kwargs["file"] = FileUploadObj
+
+    await RtiObj.update_from_dict(_kwargs)
+
+    await RtiObj.save()
+
+    return api.utils.remove_meta_rti(dict(RtiObj))
+
+
+@router.delete("/delete")
 async def delete_rti(
     content: rti.RtiDeleteIn, current_user=Depends(deps.get_current_active_user)
 ) -> Any:
 
     RtiObj = await Rti.get(id=content.id)
-    
+
     if not RtiObj:
         raise HTTPException(status_code=404, detail="Rti not found")
-    
+
     await RtiObj.delete()
 
     return {"Result": "OK"}
-
--- schemas/rti.py
+++ schemas/rti.py
@@ -14,5 +14,19 @@ class RtiIn(BaseModel):
     query: str
     file: str
 
+
+class RtiUpdateIn(BaseModel):
+    id: str
+    rti_send_date: str
+    registration_number: str
+    name_of_sender: str
+    email_of_sender: EmailStr
+    ministry: int
+    public_authority: int
+    topic: List[str]
+    query: str
+    file: str
+
+
 class RtiDeleteIn(BaseModel):
     id: str
